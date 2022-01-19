81bea3d6adaa0ee2f72b10ce651c41698d3bd69d
unknown <alngregory@gmail.com>
2022-01-18 22:57:04 -0300

    Model class created and GetById base created

--- Controllers/HashPassController.cs
+++ Controllers/HashPassController.cs
@@ -1,11 +1,20 @@
+using System.Collections;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HashPass.Controllers
 {
     public class HashPassController : ControllerBase
     {
+        //GET api/HashPass
         [HttpGet]
         public ActionResult GetAllHashPass()
+        {
+            return Ok();
+        }
+
+        //GET api/HashPass/{id}
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable> GetHashPassById(int id)
         {
             return Ok();
         }
---
+++ Models/HashPassModel.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HashPassModel.Models
+{
+    public class HashPassModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string PassText { get; set; }
+        
+        [Required]
+        public string HashPass { get; set; }
+        
+        [Required]
+        public string CreationDate { get; set; }
+
+        public string UpdatedDate { get; set; }
+    }
+}
